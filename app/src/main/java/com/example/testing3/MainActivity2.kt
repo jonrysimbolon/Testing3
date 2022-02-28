7e678d22ad6627b65ee0494428daa7b8490bc49a
Jonry Simbolon <jonrysimbolon19@gmail.com>
2022-02-28 16:17:01 +0700

    First commit

---
+++ app/src/main/java/com/example/testing3/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.testing3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
